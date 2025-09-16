4424582a5dae844c35b3520f2ffa41e62c7a09a0
Md Kaif <152685529+mdkaif098@users.noreply.github.com>
2025-09-16 14:20:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,9 @@ npm -v
 echo "⚡ Installing Irys CLI globally..."
 sudo npm i -g @irys/cli
 
+echo "✅ Verifying Irys CLI installation..."
+irys
+
 echo "🔑 Please enter your private key (without 0x):"
 read PRIVATE_KEY
 
